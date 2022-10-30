476383d13eea76c8bdc556b6ecfbef207dbfd852
Xenia Kostyushko <latentblondinko@yandex.ru>
2022-10-30 23:03:06 +0300

    updated

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    template = 'infra_app/index.html'
+    return render(request, template)
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    template = 'infra_app/second_page.html'
+    return render(request, template)
